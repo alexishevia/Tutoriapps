2e27240f1f1ea7840d8c8e4f8b49dc4fd801b8cc
Alexis Hevia <hevia.alexis@gmail.com>
2012-04-26 11:22:55 -0500

    Label now showing up right to checkbox

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -82,6 +82,12 @@ SimpleForm.setup do |config|
     end
   end
 
+  config.wrappers :checkbox, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
+    b.use :tag => 'div', :class => 'controls' do |ba|
+      ba.use :label_input
+    end
+  end
+
   # Wrappers for forms and inputs using the Twitter Bootstrap toolkit.
   # Check the Bootstrap docs (http://twitter.github.com/bootstrap)
   # to learn about the different styles for forms and inputs,
