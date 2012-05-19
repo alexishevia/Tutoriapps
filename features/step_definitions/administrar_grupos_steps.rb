f48d34890ec23f16e13c791b4d2ba040dd29df62
Alexis Hevia <hevia.alexis@gmail.com>
2012-05-19 14:58:50 -0500

    Enhanced tests to expose bugs

--- features/step_definitions/administrar_grupos_steps.rb
+++ features/step_definitions/administrar_grupos_steps.rb
@@ -41,19 +41,6 @@ Cuando /^intente crear el grupo "([^\"]*)"$/ do |group_name|
   end
 end
 
-Entonces /^el grupo "([^\"]*)" aparecerá (\d+) (vez|veces) en la lista de grupos$/ do |group_name, n, arg3|
-  within ".groups" do
-    page.should have_content(group_name)
-    page.text.scan(group_name).length.should eq(n.to_i)
-  end
-end
-
-Entonces /^el grupo "([^\"]*)" no aparecerá en la lista de grupos$/ do |group_name|
-  within ".groups" do
-    page.should_not have_content(group_name)
-  end
-end
-
 Cuando /^intente agregar el usuario "([^"]*)" al grupo "([^"]*)"$/ do |user_email, group_name|
   group = Group.find_by_name(group_name)
   visit root_path
@@ -64,33 +51,6 @@ Cuando /^intente agregar el usuario "([^"]*)" al grupo "([^"]*)"$/ do |user_emai
     find(".new_enrollment input[placeholder='#{ I18n.t(
       'activerecord.attributes.enrollment.user_email') }']").set(user_email)
     click_button I18n.t('helpers.submit.send')
-  end
-end
-
-Entonces /^el usuario "([^"]*)" aparecerá dentro del grupo "([^"]*)"$/ do |user_email, group_name|
-  group = Group.find_by_name(group_name)
-  user = User.find_by_email(user_email) || User.new(name: user_email)
-  within find_link(group_name).find(:xpath,".//ancestor::*[contains(@class, 'group')]") do
-    page.should have_content(user.name)
-  end
-end
-
-Entonces /^el usuario "([^\"]*)" no aparecerá dentro del grupo "([^\"]*)"$/ do |user_email, group_name|
-  group = Group.find_by_name(group_name)
-  user = User.find_by_email(user_email) || User.new(name: user_email)
-  within find_link(group_name).find(:xpath,".//ancestor::*[contains(@class, 'group')]") do
-    page.should_not have_content(user.name)
-  end
-end
-
-Entonces /^recibirá el status (\d+)$/ do |code|
-  last_response.status.should eq(code.to_i)
-end
-
-Entonces /^el usuario "([^"]*)" aparecerá (\d+) vez en el grupo "([^\"]*)"$/ do |user_email, n, group_name|
-  user = User.find_by_email(user_email) || User.new(name: user_email)
-  within find_link(group_name).find(:xpath,".//ancestor::*[contains(@class, 'group')]") do
-    page.find('.members').text.scan(user.name).length.should eq(n.to_i)
   end
 end
 
@@ -114,5 +74,69 @@ end
 Cuando /^intente eliminar el grupo "([^\"]*)"$/ do |group_name|
   within find_link(group_name).find(:xpath,".//..") do
     page.find('.delete_group').click
+  end
+end
+
+Entonces /^el grupo "([^\"]*)" aparecerá (\d+) (vez|veces) en la lista de grupos$/ do |group_name, n, arg3|
+  within ".groups" do
+    page.should have_content(group_name)
+    page.text.scan(group_name).length.should eq(n.to_i)
+  end
+  visit root_path
+  within ".groups" do
+    page.should have_content(group_name)
+    page.text.scan(group_name).length.should eq(n.to_i)
+  end
+end
+
+Entonces /^el grupo "([^\"]*)" no aparecerá en la lista de grupos$/ do |group_name|
+  within ".groups" do
+    page.should_not have_content(group_name)
+  end
+  visit root_path
+  within ".groups" do
+    page.should_not have_content(group_name)
+  end
+end
+
+Entonces /^el usuario "([^"]*)" aparecerá dentro del grupo "([^"]*)"$/ do |user_email, group_name|
+  group = Group.find_by_name(group_name)
+  user = User.find_by_email(user_email) || User.new(name: user_email)
+  within find_link(group_name).find(:xpath,".//ancestor::*[contains(@class, 'group')]") do
+    page.should have_content(user.name)
+  end
+  visit root_path
+  within find_link(group_name).find(:xpath,".//ancestor::*[contains(@class, 'group')]") do
+    click_link(group_name)
+    page.should have_content(user.name)
+  end
+end
+
+Entonces /^el usuario "([^\"]*)" no aparecerá dentro del grupo "([^\"]*)"$/ do |user_email, group_name|
+  group = Group.find_by_name(group_name)
+  user = User.find_by_email(user_email) || User.new(name: user_email)
+  within find_link(group_name).find(:xpath,".//ancestor::*[contains(@class, 'group')]") do
+    page.should_not have_content(user.name)
+  end
+  visit root_path
+  within find_link(group_name).find(:xpath,".//ancestor::*[contains(@class, 'group')]") do
+    click_link(group_name)
+    page.should_not have_content(user.name)
+  end
+end
+
+Entonces /^recibirá el status (\d+)$/ do |code|
+  last_response.status.should eq(code.to_i)
+end
+
+Entonces /^el usuario "([^"]*)" aparecerá (\d+) vez en el grupo "([^\"]*)"$/ do |user_email, n, group_name|
+  user = User.find_by_email(user_email) || User.new(name: user_email)
+  within find_link(group_name).find(:xpath,".//ancestor::*[contains(@class, 'group')]") do
+    page.find('.members').text.scan(user.name).length.should eq(n.to_i)
+  end
+  visit root_path
+  within find_link(group_name).find(:xpath,".//ancestor::*[contains(@class, 'group')]") do
+    click_link(group_name)
+    page.find('.members').text.scan(user.name).length.should eq(n.to_i)
   end
 end
