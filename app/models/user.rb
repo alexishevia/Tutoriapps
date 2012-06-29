e9df4a6bf1dd48a6c9d5fa1cd1d2744529707c3b
Alexis Hevia <hevia.alexis@gmail.com>
2012-06-28 20:38:11 -0500

    Fixes #16

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,26 +2,30 @@
 #
 # Table name: users
 #
-#  id                     :integer(4)      not null, primary key
-#  email                  :string(255)     default(""), not null
-#  encrypted_password     :string(255)     default(""), not null
-#  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer(4)      default(0)
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
-#  confirmation_token     :string(255)
-#  confirmed_at           :datetime
-#  confirmation_sent_at   :datetime
-#  unconfirmed_email      :string(255)
-#  authentication_token   :string(255)
-#  created_at             :datetime        not null
-#  updated_at             :datetime        not null
-#  name                   :string(255)
-#  admin                  :boolean(1)      default(FALSE), not null
+#  id                       :integer(4)      not null, primary key
+#  email                    :string(255)     default(""), not null
+#  encrypted_password       :string(255)     default(""), not null
+#  reset_password_token     :string(255)
+#  reset_password_sent_at   :datetime
+#  remember_created_at      :datetime
+#  sign_in_count            :integer(4)      default(0)
+#  current_sign_in_at       :datetime
+#  last_sign_in_at          :datetime
+#  current_sign_in_ip       :string(255)
+#  last_sign_in_ip          :string(255)
+#  confirmation_token       :string(255)
+#  confirmed_at             :datetime
+#  confirmation_sent_at     :datetime
+#  unconfirmed_email        :string(255)
+#  authentication_token     :string(255)
+#  created_at               :datetime        not null
+#  updated_at               :datetime        not null
+#  name                     :string(255)
+#  admin                    :boolean(1)      default(FALSE), not null
+#  profile_pic_file_name    :string(255)
+#  profile_pic_content_type :string(255)
+#  profile_pic_file_size    :integer(4)
+#  profile_pic_updated_at   :datetime
 #
 
 class User < ActiveRecord::Base
@@ -32,7 +36,7 @@ class User < ActiveRecord::Base
          :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :profile_pic
 
   validates :email, :format => { :with => /@utp.ac.pa$/, :message =>
     "#{I18n.t('errors.messages.invalid')} - " +
@@ -42,6 +46,8 @@ class User < ActiveRecord::Base
   has_many :posts, :dependent => :destroy
   has_many :enrollments, :dependent => :destroy
   has_many :groups, :through => :enrollments
+  has_attached_file :profile_pic, :styles => { :thumb => "100x100#" },
+    :default_url => '/assets/unknown-user.png'
 
   after_create :reload_enrollments
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120623193246) do
+ActiveRecord::Schema.define(:version => 20120629010808) do
 
   create_table "board_pics", :force => true do |t|
     t.string   "image_file_name"
@@ -87,12 +87,12 @@ ActiveRecord::Schema.define(:version => 20120623193246) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "email",                    :default => "",    :null => false
+    t.string   "encrypted_password",       :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",            :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -102,10 +102,14 @@ ActiveRecord::Schema.define(:version => 20120623193246) do
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.string   "authentication_token"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
     t.string   "name"
-    t.boolean  "admin",                  :default => false, :null => false
+    t.boolean  "admin",                    :default => false, :null => false
+    t.string   "profile_pic_file_name"
+    t.string   "profile_pic_content_type"
+    t.integer  "profile_pic_file_size"
+    t.datetime "profile_pic_updated_at"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
